82f8ea124ebc2afe3f1ff3339bfdcd6c501d6b14
Shivam Veerabudren <92755248+Shiv2205@users.noreply.github.com>
2023-04-03 10:04:19 -0400

    ..

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require("express");
-const util = require('./util/handleCORS');
+const util = require("./util/handleCORS");
 
 const apiEndpoints = require("./endpoints/api-endpoints");
 
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', '*');//GET, POST, ...
-    res.setHeader('Access-Control-Allow-Headers', '*'); //Content-Type, Authorization
-    next();
+    res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT"); //GET, POST, ...
+  //res.setHeader('Access-Control-Allow-Headers', '*'); //Content-Type, Authorization
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  res.status(200);
+  next();
 });
 
 app.use("/api-endpoints", apiEndpoints);
