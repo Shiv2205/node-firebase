7f54fa9f2face4ba2e1ced092df33251eff3e790
Shivam Veerabudren <92755248+Shiv2205@users.noreply.github.com>
2023-06-04 10:17:22 -0400

    ...

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const app = express();
 
 app.use(helmet());
 
-app.all('/api-endpoints/*', (req, res, next) => {
+app.all('/*', (req, res, next) => {
   res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", '*');
+  res.setHeader("Access-Control-Allow-Origin", 'https://wedding-app-ebon.vercel.app');
   res.removeHeader('x-powered-by');
   res.setHeader("Access-Control-Allow-Methods", req.method); //"GET,OPTIONS,POST,DELETE,PATCH,PUT"
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); //Content-Type, Authorization
